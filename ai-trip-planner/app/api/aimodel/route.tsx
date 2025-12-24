07364c6e84534b0d1d7dfd2da3617aca90563c44
Jagdish Uikey <juikey697@gmail.com>
2025-12-24 10:00:32 +0530

    applecoruosel

--- ai-trip-planner/app/api/aimodel/route.tsx
+++ ai-trip-planner/app/api/aimodel/route.tsx
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import OpenAI from 'openai';
 export const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
-    apiKey: process.env.OPENROUTER_API_KEY,
+    apiKey: process.env.VITE_GEMINI_API_KEY,
 
 });
 
